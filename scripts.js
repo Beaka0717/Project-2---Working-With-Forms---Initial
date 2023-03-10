c13f76d7a9b40249f24a8674a6b468941863ee5a
Beaka0717 <minjee9009@gmail.com>
2023-03-10 17:59:45 +0900

    xcsxc

---
+++ scripts.js
@@ -0,0 +1,32 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Acoustic Guitar'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Electric Guitar'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember: {
+                
+            }
+        }
+    },
+    methods:{
+        addMember: function(){
+            this.members.push(this.newMember);
+        }
+    }
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
